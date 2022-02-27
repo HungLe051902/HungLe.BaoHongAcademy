c25a9c078770439a9fa7c18cc2f18bcd54505335
Hưng Lê <lexuanhung020599@gmail.com>
2022-02-27 18:28:33 +0700

    Add some items

---
+++ BaoHongAcademy.Domain/Constants/CommonConstant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaoHongAcademy.Domain.Constants
+{
+    public class CommonConstant
+    {
+    }
+}
