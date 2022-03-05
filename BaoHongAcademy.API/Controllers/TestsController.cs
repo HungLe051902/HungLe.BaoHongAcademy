e9edd291d94450e7b36285d53b00a4986be4542f
Bui Quang Trung <bui.quangtrung@petronas.com>
2022-03-05 20:25:18 +0700

    Handle insert DB

--- BaoHongAcademy.API/Controllers/TestsController.cs
+++ BaoHongAcademy.API/Controllers/TestsController.cs
@@ -1,6 +1,7 @@
 ﻿using BaoHongAcademy.Domain.Entities;
 using BaoHongAcademy.Infrastructure.DataLayer;
 using Microsoft.AspNetCore.Mvc;
+using static BaoHongAcademy.Domain.Enums.EnumCommon;
 
 namespace BaoHongAcademy.API.Controllers
 {
@@ -13,9 +14,18 @@ namespace BaoHongAcademy.API.Controllers
         {
             using (var dbCon = new DatabaseConnection())
             {
-                //dbCon.GetData<>
-                var list = dbCon.GetDataRawSql<Blog>("select * from Blog Where Author = 'Hung';", null);
-                var a = 1;
+                ////dbCon.GetData<>
+                //var list = dbCon.GetDataRawSql<Blog>("select * from Blog Where Author = 'Hung';", null);
+                //var a = 1;
+
+                var blog = new Blog()
+                {
+                    Author = "Nam",
+                    BlogType = BlogType.IT,
+                    BlogContent = "Nam"
+                };
+
+                dbCon.InsertIntoDB<Blog>("Proc_InsertBlog", blog);
             }
         }
     }
--- BaoHongAcademy.Infrastructure/DataLayer/DatabaseConnection.cs
+++ BaoHongAcademy.Infrastructure/DataLayer/DatabaseConnection.cs
@@ -1,15 +1,20 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using BaoHongAcademy.Domain.Entities;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Globalization;
+using System.Reflection;
 
 namespace BaoHongAcademy.Infrastructure.DataLayer
 {
-    public class DatabaseConnection : IDisposable
+    public class DatabaseConnection : IDatabaseConnection
     {
         private readonly string connectionStr = "Data Source=DESKTOP-R7RA7TV; Initial Catalog=BaoHongAcademyDevelopment; Integrated Security=SSPI; TrustServerCertificate=True";
         private readonly SqlConnection sqlConnection;
         private readonly SqlCommand sqlCommand;
+
+        public string ConnectionString { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
         public DatabaseConnection()
         {
@@ -24,46 +29,15 @@ namespace BaoHongAcademy.Infrastructure.DataLayer
             command.CommandType = commandType;
         }
 
-        public IEnumerable<T> GetData<T>(string procedureName)
+        public IEnumerable<T> GetData<T>(string procedureName, IDictionary<string, object> parameters = null) where T : class
         {
             sqlCommand.CommandType = CommandType.StoredProcedure;
             sqlCommand.CommandText = procedureName;
-            using var sqlDataReader = sqlCommand.ExecuteReader();
 
-            var result = new List<T>();
-            while(sqlDataReader.Read())
+            foreach (var param in parameters)
             {
-                var item = Activator.CreateInstance<T>();
-                foreach (var property in typeof(T).GetProperties())
-                {
-                    if (!sqlDataReader.IsDBNull(sqlDataReader.GetOrdinal(property.Name)))
-                    {
-                        var dataDB = sqlDataReader[property.Name];
-                        var value = (dynamic)null;
-                        Type convertTo = Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType;
-                        if (convertTo.IsEnum)
-                        {
-                            value = Enum.ToObject(convertTo, dataDB);
-                        }
-                        else
-                        {
-                            value = Convert.ChangeType(dataDB, convertTo);
-                        }
-                        property.SetValue(item, value, null);
-                    }
-                }
-                result.Add(item);
+                sqlCommand.Parameters.Add(new SqlParameter($"@{param.Key}", param.Value));
             }
-
-            return result;
-        }
-
-        public IEnumerable<T> GetDataRawSql<T>(string sql, object[] parameters)
-        {
-            sqlCommand.CommandType = CommandType.Text;
-            sqlCommand.CommandText = sql;
-
-            sqlCommand.Parameters.Add(new SqlParameter("@in_Author", "Hung"));
 
             using var sqlDataReader = sqlCommand.ExecuteReader();
 
@@ -95,9 +69,67 @@ namespace BaoHongAcademy.Infrastructure.DataLayer
             return result;
         }
 
-        public void MapObjectAndParameter()
+        public IEnumerable<T> GetDataRawSql<T>(string sql, IDictionary<string, object> parameters = null) where T : class
         {
+            sqlCommand.CommandType = CommandType.Text;
+            sqlCommand.CommandText = sql;
 
+            foreach (var param in parameters)
+            {
+                sqlCommand.Parameters.Add(new SqlParameter($"@{param.Key}", param.Value));
+            }
+
+            using var sqlDataReader = sqlCommand.ExecuteReader();
+
+            var result = new List<T>();
+            while (sqlDataReader.Read())
+            {
+                var item = Activator.CreateInstance<T>();
+                foreach (var property in typeof(T).GetProperties())
+                {
+                    if (!sqlDataReader.IsDBNull(sqlDataReader.GetOrdinal(property.Name)))
+                    {
+                        var dataDB = sqlDataReader[property.Name];
+                        var value = (dynamic)null;
+                        Type convertTo = Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType;
+                        if (convertTo.IsEnum)
+                        {
+                            value = Enum.ToObject(convertTo, dataDB);
+                        }
+                        else
+                        {
+                            value = Convert.ChangeType(dataDB, convertTo);
+                        }
+                        property.SetValue(item, value, null);
+                    }
+                }
+                result.Add(item);
+            }
+
+            return result;
+        }
+        public void MapEntityAndParameter<T>(List<SqlParameter> sqlParameters, T entity) where T : BaseEntity
+        {
+            if (entity is null) return;
+
+            foreach (var property in typeof(T).GetProperties())
+            {
+                var value = property.GetValue(entity, BindingFlags.Public | BindingFlags.Instance, null, null, CultureInfo.InvariantCulture);
+                sqlParameters.Add(new SqlParameter($"@in_{property.Name}", value ?? DBNull.Value));
+            }
+        }
+
+        public int InsertIntoDB<T>(string storeName, T entity) where T : BaseEntity
+        {
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.CommandText = storeName;
+
+            var sqlParameters = new List<SqlParameter>();
+            MapEntityAndParameter<T>(sqlParameters, entity);
+            sqlCommand.Parameters.AddRange(sqlParameters.ToArray());
+
+            var result = sqlCommand.ExecuteNonQuery();
+            return result;
         }
 
         public void Dispose()
@@ -107,6 +139,21 @@ namespace BaoHongAcademy.Infrastructure.DataLayer
                 sqlConnection.Close();
             }
             sqlConnection.Dispose();
+        }
+
+        public void Open()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BeginTransaction()
+        {
+            throw new NotImplementedException();
         }
     }
 }
