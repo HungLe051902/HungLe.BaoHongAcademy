d24d6325bcc0f2560d4fcb623027faad51493d43
Nam Tran Van <nam.tranvan@petronas.com>
2022-05-07 08:30:54 +0700

    Remove packge

--- BaoHongAcademy.WebApp/wwwroot/src/main.js
+++ BaoHongAcademy.WebApp/wwwroot/src/main.js
@@ -1,31 +1,28 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { IonicVue } from "@ionic/vue";
+// import { IonicVue } from "@ionic/vue";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 /* Core CSS required for Ionic components to work properly */
-import "@ionic/vue/css/core.css";
+// import "@ionic/vue/css/core.css";
 
-/* Basic CSS for apps built with Ionic */
-import "@ionic/vue/css/normalize.css";
-import "@ionic/vue/css/structure.css";
-import "@ionic/vue/css/typography.css";
+// /* Basic CSS for apps built with Ionic */
+// import "@ionic/vue/css/normalize.css";
+// import "@ionic/vue/css/structure.css";
+// import "@ionic/vue/css/typography.css";
 
-/* Optional CSS utils that can be commented out */
-import "@ionic/vue/css/padding.css";
-import "@ionic/vue/css/float-elements.css";
-import "@ionic/vue/css/text-alignment.css";
-import "@ionic/vue/css/text-transformation.css";
-import "@ionic/vue/css/flex-utils.css";
-import "@ionic/vue/css/display.css";
+// /* Optional CSS utils that can be commented out */
+// import "@ionic/vue/css/padding.css";
+// import "@ionic/vue/css/float-elements.css";
+// import "@ionic/vue/css/text-alignment.css";
+// import "@ionic/vue/css/text-transformation.css";
+// import "@ionic/vue/css/flex-utils.css";
+// import "@ionic/vue/css/display.css";
 
 /* Import css */
 import "@/assets/scss/main.scss";
-
-/* Import tailwindcss */
-// import "./index.css";
 
 /* Vue-toastification */
 import Notifications from "@kyvg/vue3-notification";
@@ -66,7 +63,7 @@ const app = createApp({
 
 app.use(router);
 app.use(ElementPlus);
-app.use(IonicVue);
+// app.use(IonicVue);
 app.use(Notifications);
 app.component("font-awesome-icon", FontAwesomeIcon);
 app.use(GAuth, gAuthOptions);
